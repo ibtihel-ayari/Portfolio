997107dac684213e52c02ecae0c010d10e775d3f
IbtihelAyari <ibtihelayari88@gmail.com>
2026-07-28 11:20:35 +0100

    footer commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { Hero } from './components/hero/hero';   // adapte le chemin/nom si besoin
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Hero],          // on déclare le composant utilisé
+  imports: [Header, Hero, About, Skills, Projects, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
